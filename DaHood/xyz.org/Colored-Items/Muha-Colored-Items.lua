6654709c19b429d2e936b9e6f1f1cd5410d61ef5
15yrold <101235849+15yrold@users.noreply.github.com>
2022-06-01 12:13:48 -0400

    Update Muha-Colored-Items.lua

--- DaHood/xyz.org/Colored-Items/Muha-Colored-Items.lua
+++ DaHood/xyz.org/Colored-Items/Muha-Colored-Items.lua
@@ -1,16 +1,13 @@
-local customitems = true
 local Material = 'ForceField'
 local Colour = Color3.fromRGB(87, 11, 163)
 local rservice = game:GetService('RunService')
 local backpack = game.Players.LocalPlayer.Backpack
 
-if customitems == true then
-    rservice.RenderStepped:Connect(function()
-        for i, v in pairs(backpack:GetDescendants()) do
-            if v:IsA('BasePart') then
-            v.Material = Material
-            v.Color = Colour
-            end
+rservice.RenderStepped:Connect(function()
+    for i, v in pairs(backpack:GetDescendants()) do
+        if v:IsA('BasePart') then
+        v.Material = Material
+        v.Color = Colour
         end
-    end)
-end
+    end
+end)
