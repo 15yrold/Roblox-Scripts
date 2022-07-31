f2948fbb6399feccffecf8361900e55ca145d8ba
15yrold <101235849+15yrold@users.noreply.github.com>
2022-07-31 02:28:20 -0400

    Create Fog.lua

---
+++ HoodModded/Allah/Allah-Separated/Fog.lua
@@ -0,0 +1,6 @@
+local LightingService = game:GetService('Lighting')
+
+LightingService.FogColor = Color3.fromRGB(104, 0, 255)
+LightingService.FogEnd = 1000
+LightingService.FogStart = 0
+LightingService.Ambient = Color3.fromRGB(44,44,44)
